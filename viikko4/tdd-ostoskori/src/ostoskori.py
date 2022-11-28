4a6e99a4ef81bab7dcc6ea4a485a40195f5182cb
Vili Sinervä <vili.m.sinerva@gmail.com>
2022-11-28 19:12:41 +0200

    Task5-5 Price is right after adding two different products

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -13,11 +13,10 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        if self.tavaroita_korissa() > 0:
-            ostos = list(self._tuotteet.values())[0]
-            return ostos.hinta()
-        else:
-            return 0
+        summa = 0
+        for _, tuote in self._tuotteet.items():
+            summa += tuote.hinta()
+        return summa
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
