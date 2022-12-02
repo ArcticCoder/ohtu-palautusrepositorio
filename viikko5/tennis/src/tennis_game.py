353bef6eda67448f6487a9d53f85a717abac1f41
Vili Sinervä <vili.m.sinerva@gmail.com>
2022-12-02 20:17:34 +0200

    Week 5 task 3

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -2,56 +2,61 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
-        self.m_score1 = 0
-        self.m_score2 = 0
+        self.score1 = 0
+        self.score2 = 0
+
+        self.result_text = ""
+
+        self._advantage_cutoff = 4
+        self._score_diff_to_win = 2
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.m_score1 = self.m_score1 + 1
+        if player_name == self.player1_name:
+            self.score1 = self.score1 + 1
         else:
-            self.m_score2 = self.m_score2 + 1
+            self.score2 = self.score2 + 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        self.result_text = ""
+        self._score_to_result_call()
+        return self.result_text 
 
-        if self.m_score1 == self.m_score2:
-            if self.m_score1 == 0:
-                score = "Love-All"
-            elif self.m_score1 == 1:
-                score = "Fifteen-All"
-            elif self.m_score1 == 2:
-                score = "Thirty-All"
-            elif self.m_score1 == 3:
-                score = "Forty-All"
-            else:
-                score = "Deuce"
-        elif self.m_score1 >= 4 or self.m_score2 >= 4:
-            minus_result = self.m_score1 - self. m_score2
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+    def _score_to_result_call(self):
+        if self.score1 == self.score2:
+            self._handle_tie()
+        elif max(self.score1, self.score2) >= self._advantage_cutoff:
+            self._handle_advantage_play()
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.m_score1
-                else:
-                    score = score + "-"
-                    temp_score = self.m_score2
+            self._handle_normal_points()
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def _handle_tie(self):
+        if self.score1 < self._advantage_cutoff:
+            self.result_text = "-All"
 
-        return score
+        call = self._points_to_call(self.score1)
+        self.result_text = call + self.result_text
+
+    def _handle_advantage_play(self):
+        score_diff = self.score1 - self. score2
+
+        if score_diff > 0:
+            lead_player = self.player1_name
+        else:
+            lead_player = self.player2_name
+
+        self.result_text = f"Advantage {lead_player}"
+
+        if abs(score_diff) >= self._score_diff_to_win:
+            self.result_text = f"Win for {lead_player}"
+
+    def _handle_normal_points(self):
+        player1_call = self._points_to_call(self.score1)
+        player2_call = self._points_to_call(self.score2)
+
+        self.result_text = player1_call + "-" + player2_call
+
+    def _points_to_call(self, points):
+        mapping = {0:"Love", 1:"Fifteen", 2:"Thirty", 3:"Forty", 4:"Deuce"}
+        if points in mapping:
+            return mapping[points]
+        return ""
