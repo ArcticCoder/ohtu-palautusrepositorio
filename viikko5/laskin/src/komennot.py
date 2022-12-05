cebf40101a235ff7d9dc46426b2b48a0d7098168
Vili Sinervä <vili.m.sinerva@gmail.com>
2022-12-05 16:59:25 +0200

    Week 5 task 5

--- viikko5/laskin/src/komennot.py
+++ viikko5/laskin/src/komennot.py
@@ -6,44 +6,62 @@ class Summa:
     def __init__(self, sovellus, lue_arvo):
         self._sovellus = sovellus
         self._lue_arvo = lue_arvo
+        self._arvo = 0
 
     def suorita(self):
-        arvo = 0
+        self._arvo = 0
         try:
-            arvo = int(self._lue_arvo())
+            self._arvo = int(self._lue_arvo())
         except Exception:
             pass
-        self._sovellus.plus(arvo)
+        self._sovellus.plus(self._arvo)
+
+    def kumoa(self):
+        self._sovellus.miinus(self._arvo)
+        self._arvo = 0
 
 class Erotus:
     def __init__(self, sovellus, lue_arvo):
         self._sovellus = sovellus
         self._lue_arvo = lue_arvo
+        self._arvo = 0
 
     def suorita(self):
-        arvo = 0
+        self._arvo = 0
         try:
-            arvo = int(self._lue_arvo())
+            self._arvo = int(self._lue_arvo())
         except Exception:
             pass
-        self._sovellus.miinus(arvo)
+        self._sovellus.miinus(self._arvo)
+
+    def kumoa(self):
+        self._sovellus.plus(self._arvo)
+        self._arvo = 0
 
 class Nollaus:
     def __init__(self, sovellus, lue_arvo):
         self._sovellus = sovellus
-        self._lue_arvo = lue_arvo
+        self._vanha_arvo = 0
 
     def suorita(self):
+        self._vanha_arvo = self._sovellus.tulos
         self._sovellus.nollaa()
 
+    def kumoa(self):
+        self._sovellus.plus(self._vanha_arvo)
+        self._vanha_arvo = 0
+
 class Kumoa:
-    def __init__(self, sovellus, lue_arvo):
+    def __init__(self, sovellus, edellinen_komento):
         self._sovellus = sovellus
-        self._lue_arvo = lue_arvo
+        self._kumottava_komento = edellinen_komento
 
     def suorita(self):
-        pass
+        self._kumottava_komento.kumoa()
 
 class Tuntematon:
     def suorita(self):
+        pass
+
+    def kumoa(self):
         pass
--- viikko5/laskin/src/komentotehdas.py
+++ viikko5/laskin/src/komentotehdas.py
@@ -5,16 +5,21 @@ class Komentotehdas:
     def __init__(self, sovellus, lue_arvo):
         self._sovellus = sovellus
         self._lue_arvo = lue_arvo 
+        self._edellinen_komento = None
 
         self._komennot = {
                 "summa": komennot.Summa(self._sovellus, self._lue_arvo),
                 "erotus": komennot.Erotus(self._sovellus, self._lue_arvo),
                 "nollaus": komennot.Nollaus(self._sovellus, self._lue_arvo),
-                "kumoa": komennot.Kumoa(self._sovellus, self._lue_arvo),
                 }
 
     def hae(self, komento):
         if komento in self._komennot:
-            return self._komennot[komento]
+            komento_olio = self._komennot[komento]
+        elif komento == "kumoa":
+            return komennot.Kumoa(self._sovellus, self._edellinen_komento)
+        else:
+            komento_olio = komennot.Tuntematon()
 
-        return komennot.Tuntematon()
+        self._edellinen_komento = komento_olio
+        return komento_olio
